730dec9be7618f701a1d1ceaa9216e9467af9b68
Bob Stauss <rstauss@burlingtoncodeacademy>
2021-10-11 09:03:30 -0400

    better intro message

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+const readline = require("readline");
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -10,11 +13,8 @@ function ask(questionText) {
 start();
 
 async function start() {
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
+  const welcomeMessage = `Hello adventurer and welcome, to your DOOM!`;
   let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+  console.log("Now write your code to make this work!");
   process.exit();
 }
